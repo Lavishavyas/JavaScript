37a65a6c3f2e46675694ac12cd6403a6752621cb
Lavishavyas <165463124+Lavishavyas@users.noreply.github.com>
2025-09-13 17:42:05 +0530

    Add files via upload

--- 04_objects.js
+++ 04_objects.js
@@ -56,3 +56,28 @@ console.log(Object.entries(tinderUser));
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+const course = {
+    coursename : "js in hind",
+    price : "999",
+    courseInstructor : "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name" : "Lavisha",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
