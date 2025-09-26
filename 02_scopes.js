2fbe7ace17a59d628429a8d8251a91d3ed592601
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-09-26 23:44:07 +0530

    Add files via upload

---
+++ 02_scopes.js
@@ -0,0 +1,15 @@
+// var c = 30
+let a = 300
+if(true) {
+    let a = 10
+    const b = 20
+    console.log("INNER : ", a);
+}
+
+// for(let i = 0; i<array.length(); i++) {
+//     const element = arr[i];
+// }
+
+console.log(a);
+// console.log(b);
+// console.log(c);
