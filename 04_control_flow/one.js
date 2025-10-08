903fdc3cb2a574ac14cb246b038800b9d9dee1a6
Lavisha Vyas <165463124+Lavishavyas@users.noreply.github.com>
2025-10-08 19:00:50 +0530

    Update one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,1 +1,54 @@
+//if
+
+const isUserloggedIn = true;
+const temparature = 41;
+
+// if(temparature === 41) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temparature is greater than 50");
+// }
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==,
+
+// const score = 200;
+// if(score > 100) {
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// }
+// else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900) {
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+const userLoggeIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+
+if(userLoggeIn && debitCard && 2==3) {
+    console.log("Allow to buy course")
+}
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
 
